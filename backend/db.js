c7be90c0967e5ab4664fdbd29524b0dbe0e76c8b
123dana123 <12231924@students.liu.edu.lb>
2025-12-31 21:28:49 +0200

    Prepare backend for deployment with env variables

--- backend/db.js
+++ backend/db.js
@@ -1,12 +1,13 @@
-const mysql= require("mysql2/promise");
+const mysql = require("mysql2/promise");
 
 const pool = mysql.createPool({
-    host:"localhost",
-    user:"root",
-    password:"",
-    database:"brewhaven",
-    port: 3306,
-    waitForConnections: true,
-    connectionLimit: 10
+  host: process.env.DB_HOST || "localhost",
+  user: process.env.DB_USER || "root",
+  password: process.env.DB_PASSWORD || "",
+  database: process.env.DB_NAME || "brewhaven",
+  port: process.env.DB_PORT || 3306,
+  waitForConnections: true,
+  connectionLimit: 10
 });
-module.exports= pool;
+
+module.exports = pool;
